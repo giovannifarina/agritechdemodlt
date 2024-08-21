cf796e17588f3ccd394b160215399c0f695a4549
Giovanni Farina <gfarina@diag.uniroma1.it>
2024-08-21 19:09:41 +0200

    store contract address on JSON file

--- migrations/1_migration.js
+++ migrations/1_migration.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
 var MyContract = artifacts.require("AgritechDemo");
 
 module.exports = function(deployer) {
   // deployment steps
-  deployer.deploy(MyContract);
+  deployer.deploy(MyContract).then(function(instance) {
+    // Write the contract address to a file
+    const contractAddress = instance.address;
+    const filePath = path.join(__dirname, '..', 'contract_address.json');
+    fs.writeFileSync(filePath, JSON.stringify({ address: contractAddress }, null, 2));
+  });
 };
